a3e78f3126e6660d08fd51dc273d5bc1a1c449fb
kavinda-100 <kavindarathnayake100@gmail.com>
2026-01-11 11:52:27 +0530

    cahnge commit message

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,14 @@
 import { Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Index() {
 	return (
-		<View className="flex-1 justify-center items-center">
-			<Text className="text-5xl text-blue-500 font-bold">Welcome</Text>
+		<View className="flex-1 bg-primary">
+			<SafeAreaView className="flex-1">
+				<Text className="text-5xl text-blue-500 font-bold">
+					Welcome
+				</Text>
+			</SafeAreaView>
 		</View>
 	);
 }
