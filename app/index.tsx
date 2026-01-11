830ef71abdb08e6f83a9213c86fa4cd69c1d5033
kavinda-100 <kavindarathnayake100@gmail.com>
2026-01-11 10:50:22 +0530

    feat: update background color in Index component and extend Tailwind theme colors

--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,7 @@ import { Text, View } from 'react-native';
 
 export default function Index() {
 	return (
-		<View className="bg-black flex-1 justify-center items-center">
+		<View className="bg-primary flex-1 justify-center items-center">
 			<Text className="text-2xl text-blue-500">dev</Text>
 		</View>
 	);
