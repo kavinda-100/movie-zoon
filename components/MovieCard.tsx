e7256227241b338650e4a1638ba31846f669b2a9
kavinda-100 <kavindarathnayake100@gmail.com>
2026-01-11 19:49:52 +0530

    feat: enhance MovieCard component with star ratings and improved layout

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,11 +1,17 @@
+import { Ionicons } from '@expo/vector-icons';
 import { Link } from 'expo-router';
 import React from 'react';
-import { Image, Text, TouchableOpacity } from 'react-native';
+import { Image, Text, TouchableOpacity, View } from 'react-native';
+
+const getHowManyStarts = (rating: number) => {
+	return Math.round(rating / 2);
+};
 
 const MovieCard = ({ movie }: { movie: Movie }) => {
 	return (
 		<Link href={`/movies/${movie.id}`} asChild>
 			<TouchableOpacity className="w-[30%]">
+				{/* poster */}
 				<Image
 					source={{
 						uri: movie.poster_path
@@ -15,12 +21,38 @@ const MovieCard = ({ movie }: { movie: Movie }) => {
 					className="w-full rounded-lg h-52"
 					resizeMode="cover"
 				/>
+
+				{/* title */}
 				<Text
 					className="mt-2 text-sm font-bold text-white"
 					numberOfLines={1}
 				>
 					{movie.title}
 				</Text>
+
+				{/* stars */}
+				<View className="flex-row items-center justify-start gap-x-1">
+					{[...Array(getHowManyStarts(movie.vote_average))].map(
+						(_, index) => (
+							<Ionicons
+								key={index}
+								name="star"
+								size={10}
+								color="#FFD700"
+							/>
+						),
+					)}
+				</View>
+
+				{/* ratings and date */}
+				<View className="flex-row justify-between">
+					<Text className="text-xs text-white">
+						{movie.vote_average.toFixed(1)}
+					</Text>
+					<Text className="text-xs text-white">
+						{movie.release_date?.split('-')[0]}
+					</Text>
+				</View>
 			</TouchableOpacity>
 		</Link>
 	);
