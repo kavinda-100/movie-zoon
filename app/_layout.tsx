a409be6cf3dd1b58543c9a4bf830b4c4fde29fc2
kavinda-100 <kavindarathnayake100@gmail.com>
2026-01-13 18:48:59 +0530

    feat: add StatusBar component to hide status bar in RootLayout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,15 @@
 import TanstackProvider from '@/providers/TanstackProvider';
 import { Stack } from 'expo-router';
+import { StatusBar } from 'react-native';
 import './global.css';
 
 export default function RootLayout() {
 	return (
 		<TanstackProvider>
+			{/* hide status bar */}
+			<StatusBar hidden={true} />
+
+			{/* screen stack */}
 			<Stack>
 				<Stack.Screen name="(tabs)" options={{ headerShown: false }} />
 				<Stack.Screen
